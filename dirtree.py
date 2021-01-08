a50ee54180cfba6e5126bdd85468f937589798c8
rusty <rusty@debian>
2021-01-08 14:52:40 -0800

    updated img4.py and dirtree.py

--- dirtree.py
+++ dirtree.py
@@ -6,11 +6,9 @@ class DirTree():
 
     def files(self, dir):
         self.result = []
-        # for root, dirs, files in os.walk("/home/rusty/pics"):
         for root, dirs, files in os.walk(dir):
-            # print(root, dirs)
             if (len(files) > 0):
-                for file in files:
-                    self.result.append(root + "/" + file)
+                files = [ root + "/" + file for file in files ];
+                self.result.extend(files);
 
         return(self.result)
