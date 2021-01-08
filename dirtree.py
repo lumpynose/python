be5393b40aca1e80fd7110b57edd87ebe98ba9ed
rusty <rusty@debian>
2021-01-08 15:30:46 -0800

    added default dir

--- dirtree.py
+++ dirtree.py
@@ -1,11 +1,12 @@
 import os
 
-# makes a list of all files in a directory
+# makes a list of all files in a directory,
+# with its full pathname.
 class DirTree():
     def __init__(self):
         pass
 
-    def files(self, dir):
+    def files(self, dir = "."):
         self.result = []
         for root, dirs, files in os.walk(dir):
             if (len(files) > 0):
